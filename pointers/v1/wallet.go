76a1ce8fceb89b91f35b7a9ae1710008e50f87e6
nazevedo3 <nazevedo3@gmail.com>
2019-11-02 16:34:30 -0700

    Testing with pointers

---
+++ pointers/v1/wallet.go
@@ -0,0 +1,17 @@
+package wallet
+
+import "fmt"
+
+type Wallet struct {
+	balance int
+}
+
+func (w *Wallet) Balance() int {
+	return w.balance
+}
+
+func (w *Wallet) Deposit(amount int) {
+	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
+	w.balance += amount
+
+}
---
+++ pointers/v3/wallet.go
@@ -0,0 +1,31 @@
+package wallet
+
+import "fmt"
+
+type Bitcoin int
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+type Stringer interface {
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
+	w.balance += amount
+
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
+}
